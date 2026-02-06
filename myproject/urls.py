c295893aaa86338249cf5fed161915a7708bba4d
Imorgan Sonia <imorgansonia@gmail.com>
2026-02-06 11:42:21 +0300

    setup staticfiles url for admin dashboard at project urls.py file

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
